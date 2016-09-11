780e890a2ebb252b6ff1169c7c456c128cae691a
shahsarick <shahsarick@gmail.com>
2016-09-11 19:51:21 -0400

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,4 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+#'leicester' and 19
