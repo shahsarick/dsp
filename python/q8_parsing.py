19fb2effb3ca34d860e04683fcf1fe63e3382afb
shahsarick <shahsarick@gmail.com>
2016-09-11 21:38:36 -0400

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,4 +4,17 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
-#'leicester' and 19
+#'aston_villa' and -1 is the min difference
+with open('football.csv', 'rb') as f:
+    reader = csv.reader(f)
+    list1 = list(reader)
+    
+list1.pop(0)
+goals = [x[5] for x in list1]
+goals_allowed = [x[6]for x in list1]
+values = [int(a) - int(b) for a, b in zip(goals, goals_allowed)]
+min([abs(number) for number in values])
+print values.index(min(values))
+print min(values)
+teamlist = [x[0] for x in list1]
+print teamlist[values.index(-1)]
