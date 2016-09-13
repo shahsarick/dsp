41fc11c5bdf30ae696c8723ea8e04c25e000adbd
shahsarick <shahsarick@gmail.com>
2016-09-13 02:46:14 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -8,9 +8,6 @@ with open('faculty.csv', 'rb') as f:
     reader = csv.reader(f)
     facultylist = list(reader)   
 emails = [y[3] for y in facultylist]
-emails.pop(0)
-
-emails = [y[3] for y in facultylist]
 df3 = pd.DataFrame(emails)
 header = ["email"]
 df3.to_csv('output.csv', cols = header, index = False)
