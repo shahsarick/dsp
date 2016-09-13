dc07c6d21201f51afc3df1389c9759a64cb15750
shahsarick <shahsarick@gmail.com>
2016-09-13 02:45:43 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+'''
+write emails to a new csv file
+'''
+import string
+import pandas as pd
+
+with open('faculty.csv', 'rb') as f:
+    reader = csv.reader(f)
+    facultylist = list(reader)   
+emails = [y[3] for y in facultylist]
+emails.pop(0)
+
+emails = [y[3] for y in facultylist]
+df3 = pd.DataFrame(emails)
+header = ["email"]
+df3.to_csv('output.csv', cols = header, index = False)
