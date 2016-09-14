f19ca75097703f3c277da718a509fb1a1111582e
shahsarick <shahsarick@gmail.com>
2016-09-14 00:13:22 -0700

    Update matrix_algebra.py

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -1,3 +1,26 @@
 # Matrix Algebra
 
-PLACE YOUR CODE HERE
+import numpy as np
+from numpy import matrix
+from numpy import linalg
+
+A = matrix([[1, 2, 3],[2, 7, 4]])
+A.shape
+
+B = matrix([[1, -1],[0, 1]])
+B.shape
+
+C = matrix([[5, -1], [9, 1], [6, 0]])
+C.shape
+
+D = matrix([[3, -2, -1], [1, 2, 3]])
+D.shape
+
+u = matrix([6, 2, -3, 5])
+u.shape
+
+v = matrix([3, 5, -1, 4])
+v.shape
+
+w = matrix([[1], [8], [0], [5]])
+w.shape
