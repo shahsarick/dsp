5546dd4d8a6c685c5cc70e6e8115cd100354c34d
shahsarick <shahsarick@gmail.com>
2016-09-18 19:14:09 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -3,3 +3,19 @@ with open('faculty.csv') as f:
     f.readline()
     reader=csv.reader(f,delimiter=',')
     mydict=dict((rows[0],[rows[1], rows[2], rows[3]]) for rows in reader)
+
+#Very messy solution, definitely need more practice with dictionaries
+from nameparser import HumanName
+new_names = []
+for i in facultynames:   
+    name = HumanName(i)
+    new_names.append([name.first, name.last])
+name_df = pd.DataFrame(new_names)
+rest = [y[1:4] for y in facultylist]
+rest_df= pd.DataFrame(rest)
+result = pd.concat([name_df, rest_df], axis=1, join_axes=[name_df.index])
+result.to_csv('output2.csv', index = False)
+with open('output2.csv') as f:
+    f.readline()#ignore first line
+    reader=csv.reader(f,delimiter=',')        
+    mydict=dict([((rows[0],rows[1]), [rows[2], rows[3]]) for rows in reader])
